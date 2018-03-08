ee7766bfbc1a27289d1604906105cfcb7dde312f
Brett Shelley <brettshelley03@gmail.com>
2018-03-07 19:54:28 -0700

    assignment 3 final

--- gui.py
+++ gui.py
@@ -1,14 +1,15 @@
-import Maestro
+from Maestro import Controller
 import tkinter as tk
 import time
 import queue
+from tkinter.font import Font
 
 class Button():
     def createMove():
         global count
         count=count+1
         m = Move()
-        instance = tk.Button(frame, text="Move", width=25, height=10, command=m.popUp)
+        instance = tk.Button(frame, text="Move", width=19, height=6, command=m.popUp)
         Button.place(instance)
         m.popUp
 
@@ -16,21 +17,21 @@ class Button():
         global count
         count=count+1
         t = Turn()
-        instance = tk.Button(frame, text="Turn", width=25, height=10, command=t.popUp)
+        instance = tk.Button(frame, text="Turn", width=19, height=6, command=t.popUp)
         Button.place(instance)
 
     def createBody():
         global count
         count=count+1
         b = Body()
-        instance = tk.Button(frame, text="Body", width=25, height=10, command=b.popUp)
+        instance = tk.Button(frame, text="Body", width=19, height=6, command=b.popUp)
         Button.place(instance)
 
     def createHead():
         global count
         count=count+1
         h = Head()
-        instance = tk.Button(frame, text="Head", width=25, height=10, command=h.popUp)
+        instance = tk.Button(frame, text="Head", width=19, height=6, command=h.popUp)
         Button.place(instance)
 
     def place(instance):
@@ -63,29 +64,33 @@ class Move():
 
     def popUp(self):
         pop = tk.Toplevel()
-        pop.minsize(180, 150)
+        pop.minsize(200, 150)
         pop.title("Properties")
 
         self.var = tk.IntVar()
         forward = tk.Radiobutton(pop, text="Forward", variable=self.var, value=0, command=self.direction)
         backwards = tk.Radiobutton(pop, text="Reverse", variable=self.var, value=1, command=self.direction)
         label = tk.Label(pop, text="Duration:")
-        self.duration = tk.Spinbox(pop, width=5, from_=0, to=30, increment=0.5, command=self.duration) 
-        
+        self.durationBox = tk.Spinbox(pop, width=5, from_=0, to=30, increment=0.5, command=self.duration, font=Font(family='Helvetica', size=20)) 
+        save = tk.Button(pop, text="Save", width=10, height=2, command=pop.destroy)
+
         forward.grid(pady=15)
         backwards.grid()
         label.grid(pady=20)
-        self.duration.grid(column=2, row=2, padx=5)
+        self.durationBox.grid(column=2, row=2, padx=5)
+        save.grid(pady=20, columnspan=3)
+
+        forward.invoke()
 
     def direction(self):
-        d = self.var
+        d = self.var.get()
         if (d==0):
-            self.speed=1000
+            self.speed=5000
         elif (d==1):
-            self.speed=-1000
+            self.speed=7000
 
     def duration(self):
-        d = self.duration.get()
+        d = self.durationBox.get()
         self.sleep=d
 
 class Turn():
@@ -103,25 +108,31 @@ class Turn():
         pop.title("Properties")
         
         self.var = tk.IntVar()
+
         left = tk.Radiobutton(pop, text="Left", variable=self.var, value=0, command=self.direction)
         right = tk.Radiobutton(pop, text="Right", variable=self.var, value=1, command=self.direction)
         label = tk.Label(pop, text="Duration:")
-        self.duration = tk.Spinbox(pop, width=5, from_=0, to=30, increment=0.5, command=self.duration) 
+        self.durationBox = tk.Spinbox(pop, width=5, from_=0, to=30, increment=0.5, command=self.duration, font=Font(family='Helvetica', size=20)) 
+        save = tk.Button(pop, text="Save", width=10, height=2, command=pop.destroy)
         
         left.grid(pady=15)
         right.grid()
         label.grid(pady=20)
-        self.duration.grid(column=2, row=2, padx=5)
+        self.durationBox.grid(column=2, row=2, padx=5)
+        save.grid(pady=20, columnspan=3)
+
+        left.invoke()
 
     def direction(self):
-        d = self.var
+        d = self.var.get()
         if (d==0):
-            self.speed=1000
+            self.speed=7000
         elif (d==1):
-            self.speed=-1000
+            self.speed=5000
 
     def duration(self):
-        d = self.duration.get()
+        d = self.durationBox.get()
+        self.sleep=d
 
 class Body():
     def __init__(self):
@@ -139,16 +150,24 @@ class Body():
         self.var = tk.IntVar()
         left = tk.Radiobutton(pop, text="Left", variable=self.var, value=0, command=self.direction)
         right = tk.Radiobutton(pop, text="Right", variable=self.var, value=1, command=self.direction)
+        center = tk.Radiobutton(pop, text="Center", variable=self.var, value=2, command=self.direction)
+        save = tk.Button(pop, text="Save", width=10, height=2, command=pop.destroy)
 
         left.grid(pady=15)
         right.grid()
+        center.grid(pady=15)
+        save.grid(pady=20, padx=25)
+
+        left.invoke()
 
     def direction(self):
-        d = self.var
+        d = self.var.get()
         if (d==0):
-            self.turn=4500
+            self.turn=7500
         elif (d==1):
-            self.speed=7500
+            self.turn=4500
+        elif (d==2):
+            self.turn=self.center
 
 class Head():
     def __init__(self):
@@ -168,18 +187,25 @@ class Head():
         right = tk.Radiobutton(pop, text="Right", variable=self.var, value=1, command=self.direction)
         up = tk.Radiobutton(pop, text="Up", variable=self.var, value=2, command=self.direction)
         down = tk.Radiobutton(pop, text="Down", variable=self.var, value=3, command=self.direction)
+        center = tk.Radiobutton(pop, text="Center", variable=self.var, value=4, command=self.direction)
+        save = tk.Button(pop, text="save", width=10, height=2, command=pop.destroy)
 
         left.grid(pady=15)
         right.grid()
         up.grid(pady=15)
         down.grid()
+        center.grid(pady=15)
+        save.grid(pady=20, padx=25)
+
+        left.invoke()
 
     def direction(self):
-        d = self.var
+        d = self.var.get()
         if (d==0 or d==2):
-            self.turn=4500
+            self.turn=7500
         elif (d==1 or d==3):
-            self.speed=7500
+            self.turn=4500
+
 
 class Control():
     def run():
@@ -188,8 +214,32 @@ class Control():
         for i in range(q.qsize()):
             x = q.get()
             qTemp.put(x)
-            
+            target = 0
+
             # Maestro code to execute each command
+            if type(x) is Head:
+                if (x.var.get() == 0 or x.var.get() == 1):
+                    rob.setTarget(3, x.turn)
+                elif (x.var.get() == 2 or x.var.get() == 3):
+                    rob.setTarget(4, x.turn)
+                elif (x.var.get() == 4):
+                    rob.setTarget(3, x.center)
+                    rob.setTarget(4, x.center)
+            elif type(x) is Body:
+                target = 0
+            elif type(x) is Turn:
+                target = 2
+            elif type(x) is Move:
+                target = 1
+            
+            if (type(x) is Body):
+                rob.setTarget(target, x.turn)
+            elif (type(x) is Turn or type(x) is Move):
+                rob.setTarget(target, x.speed)
+                time.sleep(float (x.sleep))
+                rob.setTarget(target, x.stop)
+                
+            time.sleep(1)
 
         q = qTemp
 
@@ -198,27 +248,28 @@ class Control():
         for j in frame.winfo_children():
             j.destroy()
 
+rob = Controller()
 q = queue.Queue()
 count=0
 
 win = tk.Tk()
 win.title("Tango Bot")
 
-moveButton = tk.Button(win, width="20", text="Move", command=Button.createMove)
-turnButton = tk.Button(win, width="20", text="Turn", command=Button.createTurn)
-bodyButton = tk.Button(win, width="20", text="Rotate Body", command=Button.createBody)
-headButton = tk.Button(win, width="20", text="Move Head", command=Button.createHead)
+moveButton = tk.Button(win, width="15", text="Move", command=Button.createMove)
+turnButton = tk.Button(win, width="15", text="Turn", command=Button.createTurn)
+bodyButton = tk.Button(win, width="15", text="Rotate Body", command=Button.createBody)
+headButton = tk.Button(win, width="15", text="Move Head", command=Button.createHead)
 moveButton.grid(column=0, row=0, pady=10)
 turnButton.grid(column=1, row=0, pady=10)
 bodyButton.grid(column=2, row=0, pady=10)
 headButton.grid(column=3, row=0, pady=10)
 
-frame = tk.Frame(win, width=920, height=500)
+frame = tk.Frame(win, width=750, height=300)
 frame.grid(columnspan=4, row=1, padx=15, pady=15)
 frame.grid_propagate(False)
 
-reset = tk.Button(win, width="25", text="Reset", command=Control.reset)
-run = tk.Button(win, width="25", text="Run", command=Control.run)
+reset = tk.Button(win, width="20", text="Reset", command=Control.reset)
+run = tk.Button(win, width="20", text="Run", command=Control.run)
 reset.grid(columnspan=2, column=0, row=2, pady=10)
 run.grid(columnspan=2, column=2, row=2, pady=10)
 
